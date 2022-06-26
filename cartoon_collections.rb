70ab0ff064a5265418c2ecb9615c19bcecbe4de1
Kelvyn King <kelvyn.m.king@gmail.com>
2022-06-26 15:15:59 +1000

    Created required methods. Completed lab.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) {|dwarf, index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(calls)
+  calls.collect {|call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4 ? true : false}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  items.find {|item| cheese_types.include?(item)}
 end
